cb200eaba4a2737a175ce0d6cf961021adba058f
jjoseph48 <josephdiva2@gmail.com>
2026-05-07 11:10:11 +0700

    add redirect auth for admin feedback

--- application/controllers/admin/Feedback.php
+++ application/controllers/admin/Feedback.php
@@ -3,6 +3,15 @@
 
 class Feedback extends CI_Controller
 {
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->model('auth_model');
+        if(!$this->auth_model->current_user()){
+            redirect('auth/login');
+        }
+    }
+    
     public function index()
     {
         $this->load->model('feedback_model');
