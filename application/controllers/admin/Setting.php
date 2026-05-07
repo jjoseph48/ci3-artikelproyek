2c4c4b07e17edc355c0d233dbe4eed4c3ad9b8b1
jjoseph48 <josephdiva2@gmail.com>
2026-05-07 11:30:02 +0700

    add redirect auth for admin setting page

--- application/controllers/admin/Setting.php
+++ application/controllers/admin/Setting.php
@@ -3,6 +3,23 @@
 
 class Setting extends CI_Controller
 {
+    // The __construct() function is a special PHP method called automatically 
+    // when a controller class is instantiated. 
+    // It is used to set up global variables, load libraries, 
+    // or check user permissions for every method within that specific controller.
+    
+    //  You must call parent::__construct() to ensure the base CodeIgniter 
+    // features are initialized.
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->model('auth_model');
+        if(!$this->auth_model->current_user()){
+            redirect('auth/login');
+        }
+    }
+
     public function index()
     {
         $this->load->view('admin/setting.php');
